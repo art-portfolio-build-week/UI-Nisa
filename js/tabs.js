149a7ccf256ae9c2176521f332e28c4c3f36da0b
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-06-24 21:08:35 -0400

    JS for tabs

--- js/tabs.js
+++ js/tabs.js
@@ -1,16 +1,36 @@
-class TabLink{
-    constructor(tabEle){
-        this.tabEle = tabEle;
-        this.tabData = this.tabEle.dataset.tab;
-        if(this.tabData === 'all'){
-            this.cards = document.querySelectorAll('.card');
-        } else {
-            this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
-        }
-        this.cards = Array.from(this.cards).map(card => new TabCard(card));
-        this.tabEle.addEventListener('click', () => this.selectTab());
-}
-
-
-let tabs = document.querySelectorAll('.tab');
-tabs.forEach(tab => new TabLink(tab));
+class TabLink {
+    constructor(tabElement){
+      this.tabElement = tabElement;
+      this.tabData = this.tabElement.dataset.tab;
+      if(this.tabData === 'all'){
+        this.cards = document.querySelectorAll('.card');
+      } else {
+        this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
+      }
+      this.cards = Array.from(this.cards).map(card => new TabCard(card));
+      this.tabElement.addEventListener('click', () => this.selectTab());
+    }
+    
+  
+    selectTab(){
+      const tabs = document.querySelectorAll('.tab');
+      tabs.forEach(tab => tab.classList.remove('active-tab'));
+      const cards = document.querySelectorAll('.card');
+      cards.forEach(card => card.style.display = 'none');
+      this.tabElement.classList.add('active-tab');
+      this.cards.forEach(card => card.selectCard());
+    }
+  }
+  
+  class TabCard {
+    constructor(cardElement){
+      this.cardElement = cardElement;
+    }
+    selectCard(){
+      this.cardElement.style.display = 'flex';
+    }
+  
+  }
+  
+  let tabs = document.querySelectorAll('.tab');
+  tabs.forEach(tab => new TabLink(tab));
